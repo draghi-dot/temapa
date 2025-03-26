6f8d9adc5d2fd0e74c5afc0e5a7d75a73a8280cf
draghi-dot <alexrobert645@gmail.com>
2025-03-26 10:01:22 +0000

    src: citire din fisier si testare task
    Signed-off-by: draghi-dot <alexrobert645@gmail.com>

--- tema.c
+++ tema.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
 int main(){
-    printf("A mers checker ul");
+    FILE *file;
+
+    file = fopen("in/data1.in", "r");
+
+    //citire numar task
+    int task;
+    fscanf(file, "%d", &task);
+
+    //citire nr coloane si linii
+    int linii, coloane;
+    fscanf(file, "%d", &linii);
+    fscanf(file, "%d", &coloane);
+
+    //citire nr generatiilor de calculat
+    int knrgen;
+    fscanf(file, "%d", &knrgen);
+
+    //testam ce task avem de rezolvat
+    if (t == 1){
+
+
+
+
+    }
+
+
 
     return 0;
 }
